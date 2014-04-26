851b8b822f70782f8a3d661b025cf40509426aaf
Dennis Ruhe <dennis@amazingsystems.nl>
2014-04-26 10:39:55 +0200

    Modified getting logic a little and added config file

--- classes/Kohana/Texter.php
+++ classes/Kohana/Texter.php
@@ -12,15 +12,26 @@
  */
 class Kohana_Texter {
 
+	/**
+	 * Get a text from the database with a certain language
+	 *
+	 * @param   key        Key of the text
+	 * @param   translate  Grab the text with specified language,
+	 *                     can be omitted and will return I18n::lang() or default language set in config
+	 * @return  String     Text you asked for
+	 */
 	public static function get($key, $translate = TRUE)
 	{
-		$lang = '';
+		// Set default lang
+		$lang = Kohana::$config->load('texter.default');
 
+		// Override default language
 		if(is_string($translate))
 		{
 			$lang = $translate;
 		}
 
+		// Override default language with I18n language
 		if($translate === TRUE)
 		{
 			$lang = I18n::lang();
@@ -28,28 +39,22 @@ class Kohana_Texter {
 
 		$select = DB::select('text')
 			->from('texter')
-			->where('key', '=', $key);
+			->where('key', '=', $key)
+			->and_where('lang', '=', $lang);
 
-		$select = ($lang)
-			? $select->and_where('lang', '=', $lang)
-			: $select;
+		$result = $select->execute();
 
-		$select = $select->execute();
-
-		if($select->count() == 0 AND $lang != I18n::lang())
+		// If there are no results, return default language set in config
+		if($result->count() == 0 AND $lang != Kohana::$config->load('texter.default'))
 		{
-			return self::get($key, I18n::lang());
+			return self::get($key, Kohana::$config->load('texter.default'));
 		}
 
-		if($select->count() > 1)
-		{
-			return $select;
-		}
-
-		return $select->get('text');
+		// Otherwise return text column
+		return $result->get('text');
 	}
 
-	public function set($key, $lang = NULL)
+	public function set($key, $lang, $text)
 	{
 
 	}
